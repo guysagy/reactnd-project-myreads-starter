d032e16d2d25097a83503381612344b09d3f2cc5
Guy Sagy <guysagy@hotmail.com>
2017-08-10 21:41:29 -0700

    refactor: refactor the shelf rendering code

--- src/App.js
+++ src/App.js
@@ -9,27 +9,34 @@ import './App.css'
 
 class BooksApp extends React.Component {
 
-  state = { books: [
-    {id: 0, title: "To Kill a Mockingbird", authors: "Harper Lee", image: "PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api", readStatus: "Currently Reading"},
-    {id: 1, title: "Ender's Game", authors: "Orson Scott Card", image: "yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api", readStatus: "Currently Reading"},
-    {id: 2, title: "1776", authors: "David McCullough", image: "uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api", readStatus: "Want to Read"},
-    {id: 3, title: "Harry Potter and the Sorcerer's Stone", authors: "J.K. Rowling", image: "wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api", readStatus: "Want to Read"},
-    {id: 4, title: "The Hobbit", authors: "J.R.R. Tolkien", image: "pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api", readStatus: "Read"},
-    {id: 5, title: "Oh, the Places You'll Go!", authors: "Seuss", image: "1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api", readStatus: "Read"},
-    {id: 6, title: "The Adventures of Tom Sawyer", authors: "Mark Twain", image: "32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api", readStatus: "Read"}
-  ]};
+  state = {
+    books: [
+      {title: "To Kill a Mockingbird",                 authors: "Harper Lee",        imageId: "PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api", shelf: "Currently Reading"},
+      {title: "Ender's Game",                          authors: "Orson Scott Card",  imageId: "yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api", shelf: "Currently Reading"},
+      {title: "1776",                                  authors: "David McCullough",  imageId: "uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api", shelf: "Want to Read"},
+      {title: "Harry Potter and the Sorcerer's Stone", authors: "J.K. Rowling",      imageId: "wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api", shelf: "Want to Read"},
+      {title: "The Hobbit",                            authors: "J.R.R. Tolkien",    imageId: "pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api", shelf: "Read"},
+      {title: "Oh, the Places You'll Go!",             authors: "Seuss",             imageId: "1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api", shelf: "Read"},
+      {title: "The Adventures of Tom Sawyer",          authors: "Mark Twain",        imageId: "32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api", shelf: "Read"}
+    ],
+    shelfs : [
+      "Currently Reading", 
+      "Want to Read", 
+      "Read"
+    ]
+  };
 
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <ListBooks booksData={this.state.books} />
+          <ListBooks shelfsData={this.state.shelfs} booksData={this.state.books} />
         )}/>
         <Route exact path="/search" render={() => (
-          <SearchBooks />        
-        )}/>              
-      </div>    
-    )  
+          <SearchBooks />
+        )}/>
+      </div>
+    );
   }
 }
 
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -5,83 +5,65 @@ import serializeForm from 'form-serialize'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
-
 class ListBooks extends Component {
-    renderBook(book) {
-     // console.log(book);
-      const bookImageUri = "http://books.google.com/books/content?id="+book.image;
-      console.log(bookImageUri);
-      return (
-          <li key={book.id}>
-            <div className="book">
-              <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url(' + bookImageUri + ')'}}></div>
-                <div className="book-shelf-changer">
-                  <select>
-                    <option value="none" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
-                </div>
-              </div>
-              <div className="book-title">{book.title}</div>
-              <div className="book-authors">{book.authors}</div>
-            </div>
-          </li>
-        );
-    }
 
-    render() {
-      console.log(this.props);
-
-        return (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                    {
-                      this.props.booksData.filter((book)=>(book.readStatus=='Currently Reading')).map((book)=>(this.renderBook(book)))
-                    }
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                    {
-                      this.props.booksData.filter((book)=>(book.readStatus=='Want to Read')).map((book)=>(this.renderBook(book)))
-                    }
-                    </ol>
-                  </div>
-                </div>
-
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                    {
-                      this.props.booksData.filter((book)=>(book.readStatus=='Read')).map((book)=>(this.renderBook(book)))
-                    }
-                    </ol>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="open-search">
-              <Link to="/search">Add a book</Link>
+  renderBook(book,index) {
+    const bookImageUri = "http://books.google.com/books/content?id=" + book.imageId;
+    return (
+      <li key={index}>
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url(' + bookImageUri + ')'}}></div>
+            <div className="book-shelf-changer">
+              <select>
+                <option value="none" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
             </div>
           </div>
-        )
-    }
+          <div className="book-title">{book.title}</div>
+          <div className="book-authors">{book.authors}</div>
+        </div>
+      </li>
+    );
+  }
+
+  renderShelf(shelf,index) {
+    return (
+      <div className="bookshelf" key={index}>
+        <h2 className="bookshelf-title">{shelf}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+          {
+            this.props.booksData.filter((book)=>(book.shelf==shelf)).map((book,index)=>(this.renderBook(book,index)))
+          }
+          </ol>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            {this.props.shelfsData.map((shelf,index)=>(this.renderShelf(shelf, index)))}
+          </div>
+        </div>
+        <div className="open-search">
+          <Link to="/search">Add a book</Link>
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default ListBooks
