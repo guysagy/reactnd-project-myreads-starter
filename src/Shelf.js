74f3eb46698759b8e577ee81c1fac1175b6a1821
Guy Sagy <guysagy@hotmail.com>
2017-08-20 20:02:56 -0700

    feature: sort shelf books by title - makes the book order predictable and stable across page refresh

--- src/Shelf.js
+++ src/Shelf.js
@@ -6,7 +6,25 @@ import Book from './Book'
 Shelf component.
 */
 
+
+
 class Shelf extends Component {
+
+  constructor(props) {
+    super(props);
+    this.titleCompare = this.titleCompare.bind(this);
+  }
+
+  // Utility function to help sort books on shelf by title.
+  // How to sort an array based on a string property - taken from the following stack overflow post :
+  // https://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value-in-javascript
+  titleCompare(a,b) {
+    if (a.title < b.title)
+      return -1;
+    if (a.title > b.title)
+      return 1;
+    return 0;
+  }
 
   render() {
     return (
@@ -18,6 +36,7 @@ class Shelf extends Component {
               // Filter myReadBooks books to shelfs by shelf name, then map books to Book components:
               this.props.myReadBooks
               .filter((book)=>(book.shelf.toLowerCase()===this.props.name.toLowerCase().replace(/ /g, "")))
+              .sort(this.titleCompare)
               .map((book)=>(<Book key={book.id} bookInfo={book} onShelfChange={this.props.onShelfChange}/>))
             }
           </ul>
